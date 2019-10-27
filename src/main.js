5afd1230fc5b2edb734c538b1d0c53b1c5fd7986
vik62bez <vik62bez@yandex.ru>
2019-10-27 04:00:53 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import Vuetify from 'vuetify'
+import BuyModalComponent from '@/components/Shared/BuyModal'
+import * as fb from 'firebase'
+import 'vuetify/dist/vuetify.min.css'
+import './stylus/main.styl'
+// import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  // theme: {
+  //   primary: "#f44336",
+  //   secondary: "#9575CD",
+  //   accent: "#9c27b0",
+  //   error: "#f44336",
+  //   warning: "#3949AB",
+  //   info: "#2196f3",
+  //   success: "#4caf50"
+  // }
+})
+Vue.component('app-buy-modal', BuyModalComponent)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyANmXO8IY9Rs8f2uU50k-RWG-S7u1wiFDY',
+      authDomain: 'itc-ads-155ce.firebaseapp.com',
+      databaseURL: 'https://itc-ads-155ce.firebaseio.com',
+      projectId: 'itc-ads-155ce',
+      storageBucket: 'itc-ads-155ce.appspot.com',
+      messagingSenderId: '646559338640',
+      appId: '1:646559338640:web:7e5867d1e5c6ce9f908815'
+    })
+
+    fb.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$store.dispatch('autoLoginUser', user)
+      }
+    })
+
+    this.$store.dispatch('fetchAds')
+  }
+})
